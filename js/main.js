5b7c680c9c03f119bcebc08ee1a26a19304c1dc2
shutkos <shutkos@gmail.com>
2023-01-11 21:47:13 +0100

    finish desktop

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 'use strict';
 
 const tabs = document.querySelectorAll(".tab");
+const jobs = document.querySelectorAll(".job");
 
-tabs.forEach(tab => {
+tabs.forEach(onTabClick);
+
+function onTabClick(tab) {
     tab.addEventListener("click", (event) => {
-        if (tab.classList.contains("active")) {
-            tab.classList.remove("active");
-        } else {
-            tab.classList.add("active");
+        let currentTab = tab;
+        let tabId = currentTab.getAttribute("data-tab");
+        let currentJob = document.querySelector(tabId);
+
+        if (!currentTab.classList.contains("active")) {
+            tabs.forEach(tab => {
+                tab.classList.remove("active");
+            });
+            jobs.forEach(job => {
+                job.classList.remove("show");
+            });
+
+            currentTab.classList.add("active");
+            currentJob.classList.add("show");
+
         }
+
+
     });
-});
+}
+document.querySelector(".tab").click();
